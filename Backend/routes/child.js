af176b9f6a90f0ff6a96a8862e366f984fb6e91d
Abishekdevil <abishekcholan07@gmail.com>
2025-06-28 08:55:36 +0530

    completed

--- Backend/routes/child.js
+++ Backend/routes/child.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Children = require('../Schema/childSchema');
+const Children = require('../schema/childSchema');
 const verifyToken = require('../middleware/auth');
 const router = express.Router();
 
